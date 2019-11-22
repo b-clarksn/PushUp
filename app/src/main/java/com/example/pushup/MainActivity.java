cda7937bab247da32821d2e58daf5d13a1363874
bclarksn <b.clarksn@gmail.com>
2019-11-21 19:06:41 -0500

    Modified MainActivity Added new file TestAddFile.java

--- app/src/main/java/com/example/pushup/MainActivity.java
+++ app/src/main/java/com/example/pushup/MainActivity.java
@@ -11,7 +11,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private void TestUpload(){
+
+    private void TestUpload() {
         int i = 2;
+
+        int ii = 2 + 2;
+
+        int iii = i + ii;
+
     }
 }
